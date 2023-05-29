2483f21098026e09a2241bb29e34d5f7abf47a76
NguyenHung2 <hn0193348@gmail.com>
2023-05-29 10:34:08 +0700

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './pasges/dashboard/dashboard.component';
+import { ReportsComponent } from './pasges/reports/reports.component';
+import { UsersComponent } from './pasges/users/users.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: DashboardComponent },
+  { path: 'reports', component: ReportsComponent },
+  { path: 'users', component: UsersComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './pasges/dashboard/dashboard.component';
+import { ReportsComponent } from './pasges/reports/reports.component';
+import { UsersComponent } from './pasges/users/users.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    ReportsComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule,
